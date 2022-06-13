cc435fc060c94a807600eaa1bae547d731ab3549
Jafer Yousef <jaferll@gmail.com>
2022-06-14 01:00:57 +0300

    Fix linters errors

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.querySelector("#burgerMenu").addEventListener("click", collapse);
-    function collapse() {
-        document.querySelectorAll(".collapse").forEach(element => 
-        element.classList.toggle("hide"));
-        this.preventDefault();
-    }
-    document.querySelectorAll(".mobMenuItem").forEach(element => {
-      element.addEventListener("click",collapse);
-    })
+function collapse() {
+  document.querySelectorAll('.collapse').forEach((element) => element.classList.toggle('hide'));
+  document.querySelector('.mobMenu').classList.toggle('fullHeight');
+  this.preventDefault();
+}
+document.querySelector('#burgerMenu').addEventListener('click', collapse());
+document.querySelectorAll('.mobMenuItem').forEach((element) => {
+  element.addEventListener('click', collapse);
+});
