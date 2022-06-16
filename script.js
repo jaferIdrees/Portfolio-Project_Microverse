01dcc634862a99b3d5eb757e4cd096015851f6c8
Jafer Yousef <jaferll@gmail.com>
2022-06-17 01:52:07 +0300

    Fix eslint errors

--- script.js
+++ script.js
@@ -49,45 +49,17 @@ const projectsInfo = [
   },
 ];
 
-/* storageAvailable function is used to check if the localstorage is available at client side this function was coppied from https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API */
-
-function storageAvailable(type) {
-  let storage;
-  try {
-    storage = window[type];
-    const x = '__storage_test__';
-    storage.setItem(x, x);
-    storage.removeItem(x);
-    return true;
-  } catch (e) {
-    return e instanceof DOMException && (
-      // everything except Firefox
-      e.code === 22
-      // Firefox
-      || e.code === 1014
-      // test name field too, because code might not be present
-      // everything except Firefox
-      || e.name === 'QuotaExceededError'
-      // Firefox
-      || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
-      // acknowledge QuotaExceededError only if there's something already stored
-      && (storage && storage.length !== 0);
-  }
-}
-
 // Use localStorage to store form data when changed
 const contactForm = document.forms[0];
 
 contactForm.addEventListener('change', () => {
-  // if (storageAvailable(localStorage)) {
-    const dataRecord = {
-      full_name: contactForm.full_name.value,
-      email_address: contactForm.email_address.value,
-      message: contactForm.message.value,
-    };
-    const portfolioFormData = JSON.stringify(dataRecord);
-    localStorage.setItem('portfolioFormData', portfolioFormData);
-  // }
+  const dataRecord = {
+    full_name: contactForm.full_name.value,
+    email_address: contactForm.email_address.value,
+    message: contactForm.message.value,
+  };
+  const portfolioFormData = JSON.stringify(dataRecord);
+  localStorage.setItem('portfolioFormData', portfolioFormData);
 });
 
 function setContactFormData(contactFormData) {
